81d319c5ef395514009ad4d759b3998cfdd504dd
Tanisha Badgujar <Tanisha.Badgujar1306@gmail.com>
2026-04-21 14:05:18 -0700

    Fix playwright.config.js BOM and quotes

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
-﻿const { defineConfig } = require(`@playwright/test`);
+const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
-  testDir: `./tests`,
-  use: { baseURL: `http://localhost:8080` },
+  testDir: './tests',
+  use: { baseURL: 'http://localhost:8080' },
 });
