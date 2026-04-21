34e38f40b7c37e0d203bd6ab91e4f42f8f94ff6b
Tanisha Badgujar <Tanisha.Badgujar1306@gmail.com>
2026-04-21 14:09:20 -0700

    Update test to use file:// path

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,7 +1,9 @@
-﻿const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
+const path = require('path');
 
 test('button click shows message text', async ({ page }) => {
-  await page.goto('/');
+  const filePath = 'file://' + path.resolve('./index.html').replace(/\\/g, '/');
+  await page.goto(filePath);
   const message = page.locator('#message');
   await expect(message).toHaveCSS('opacity', '0');
   await page.click('#reveal-btn');
